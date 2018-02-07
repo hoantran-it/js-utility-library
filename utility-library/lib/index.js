82401fd25e4583ebed60c8da258784d646e592d8
Hoan Tran <hoantran.it@gmail.com>
2018-02-07 11:25:04 +0700

    Update version 1.0.1: shortenString, buildFilter

---
+++ utility-library/lib/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var shortenString = exports.shortenString = function shortenString(string) {
+  var maxLength = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 50;
+  var hasEllipsis = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+  if (string && string.length > maxLength) {
+    if (hasEllipsis) {
+      return string.substring(0, maxLength - 3) + '...';
+    } else {
+      return string.substring(0, maxLength);
+    }
+  }
+  return string;
+};
+
+var buildUrlFilter = exports.buildUrlFilter = function buildUrlFilter() {
+  var page = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var size = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  var sortingArr = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+
+  var filter = 'page=' + page + '&size=' + size;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = sortingArr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var sorting = _step.value;
+
+      filter = filter + '&sort=' + sorting.fieldName + ',' + sorting.order;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return filter;
+};
---
+++ utility-library/src/index.js
@@ -0,0 +1,18 @@
+export const shortenString = (string, maxLength = 50, hasEllipsis = true) => {
+  if (string && string.length > maxLength) {
+    if (hasEllipsis) {
+      return string.substring(0, maxLength - 3) + '...';
+    } else {
+      return string.substring(0, maxLength);
+    }
+  }
+  return string;
+}
+
+export const buildUrlFilter = (page = 0, size = 10, sortingArr = []) => {
+  let filter = `page=${page}&size=${size}`;
+  for (var sorting of sortingArr) {
+    filter = `${filter}&sort=${sorting.fieldName},${sorting.order}`
+  }
+  return filter;
+}
