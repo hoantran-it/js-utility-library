d658914009a34d1ded472f8d95eb06d4b7fb519c
Hoan Tran <hoantran.it@gmail.com>
2018-03-18 20:59:31 +0700

    Update version 1.0.14: convert utc to local timezone

--- utility-library/src/DateTimeUtils.js
+++ utility-library/src/DateTimeUtils.js
@@ -8,20 +8,43 @@ const DateTime = {
   DATE_TIME_SECOND_STANDARD_FORMAT: "MMM DD YYYY HH:mm:ss"
 }
 
+/**
+ * Convert to current timezone
+ * @param time: must be UTC timezone
+ * @returns {*}
+ */
 const correctTimezone = (time) => {
-  return moment(time).tz(moment.tz.guess());
+  return moment.utc(time).tz(moment.tz.guess());
 }
 
+/**
+ * Fet relative time: 2 minutes ago, 3 hours ago,...
+ * @param time: must be UTC timezone
+ * @param startOf
+ * @returns {string}
+ */
 const getRelativeTime = (time, startOf = "minute")=> {
-  return moment(time).startOf(startOf).fromNow();
+  return correctTimezone(time).startOf(startOf).fromNow();
 }
 
+/**
+ * Get date time with format
+ * @param time: must be UTC timezone
+ * @param format
+ * @returns {*}
+ */
 const getDateTime = (time, format = DateTime.DATE_TIME_SECOND_STANDARD_FORMAT)=> {
-  return moment(time).format(format);
+  return correctTimezone(time).format(format);
 }
 
+/**
+ *Correct time
+ * @param time
+ * @param object
+ * @returns {boolean|*}
+ */
 const isSame = (time, object = DateTime.DATE_TIME_SECOND_STANDARD_FORMAT)=> {
-  return moment().isSame(moment(time), "day");
+  return correctTimezone(moment.utc()).isSame(correctTimezone(time), "day");
 }
 
 export default {
