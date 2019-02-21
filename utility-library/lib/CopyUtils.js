b85b6f8b489faa06fb514cf305a052727d5482ac
Hoan Tran <hoantran.it@gmail.com>
2019-02-21 11:33:14 +0700

    Improve copy object with array

--- utility-library/lib/CopyUtils.js
+++ utility-library/lib/CopyUtils.js
@@ -9,7 +9,43 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 var deepCopy = function deepCopy(obj) {
   var clone = {};
   for (var i in obj) {
-    if (obj[i] != null && _typeof(obj[i]) == "object") clone[i] = deepCopy(obj[i]);else clone[i] = obj[i];
+    if (obj[i] != null && _typeof(obj[i]) == "object") {
+      if (Array.isArray(obj[i])) {
+        clone[i] = [];
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = obj[i][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var e = _step.value;
+
+            if ((typeof e === "undefined" ? "undefined" : _typeof(e)) == "object") {
+              clone[i].push(deepCopy(e));
+            } else {
+              clone[i].push(e);
+            }
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+      } else {
+        clone[i] = deepCopy(obj[i]);
+      }
+    } else {
+      clone[i] = obj[i];
+    }
   }
   return clone;
 };
--- utility-library/src/CopyUtils.js
+++ utility-library/src/CopyUtils.js
@@ -1,10 +1,22 @@
 const deepCopy = (obj) => {
   let clone = {};
   for (let i in obj) {
-    if (obj[i] != null && typeof(obj[i]) == "object")
-      clone[i] = deepCopy(obj[i]);
-    else
+    if (obj[i] != null && typeof obj[i] == "object") {
+      if (Array.isArray(obj[i])) {
+        clone[i] = [];
+        for (let e of obj[i]) {
+          if(typeof e == "object"){
+            clone[i].push(deepCopy(e));
+          } else {
+            clone[i].push(e);
+          }
+        }
+      } else {
+        clone[i] = deepCopy(obj[i]);
+      }
+    } else {
       clone[i] = obj[i];
+    }
   }
   return clone;
 }
