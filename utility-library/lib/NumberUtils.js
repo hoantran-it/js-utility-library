d275dd1f5d828befe6d489de864bb11ba4ddfbf3
Hoan Tran <hoantran.it@gmail.com>
2019-06-11 11:15:22 +0700

    Add get random function

--- utility-library/lib/NumberUtils.js
+++ utility-library/lib/NumberUtils.js
@@ -10,6 +10,14 @@ var separateNumber = function separateNumber() {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 };
 
+var getRandomNumber = function getRandomNumber() {
+  var min = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var max = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 9;
+
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
 exports.default = {
-  separateNumber: separateNumber
+  separateNumber: separateNumber,
+  getRandomNumber: getRandomNumber
 };
--- utility-library/src/NumberUtils.js
+++ utility-library/src/NumberUtils.js
@@ -2,6 +2,11 @@ const separateNumber = (number = 0, separator = ",") => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 }
 
+const getRandomNumber = (min = 0, max = 9) => {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
 export default {
-  separateNumber
+  separateNumber,
+  getRandomNumber
 }
